342a3b02dd8b0a3f78c8a9c495e09536033dff43
Bhavesh Shah <bhaveshkumar.shah@figmd.com>
2020-08-27 13:33:59 +0530

    Updates

---
+++ precache-manifest.0c187d49604070eae8f60a9cfed8ddc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb979afd97b98d14956b4220573150d1",
+    "url": "/shreenagari/index.html"
+  },
+  {
+    "revision": "c8f838e2777a3a0ab7f9",
+    "url": "/shreenagari/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "996b750c86d4fff9f3a7",
+    "url": "/shreenagari/static/js/2.c30cf950.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shreenagari/static/js/2.c30cf950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f838e2777a3a0ab7f9",
+    "url": "/shreenagari/static/js/main.56c8d359.chunk.js"
+  },
+  {
+    "revision": "3f5bb46cfae6a19bcd86",
+    "url": "/shreenagari/static/js/runtime-main.c7ce05fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shreenagari/static/media/logo.5d5d9eef.svg"
+  }
+]);
