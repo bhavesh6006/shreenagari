c554d641e1d56faa28db1b7ec3c1878bc97bd3e0
Bhavesh Shah <bhaveshkumar.shah@figmd.com>
2020-08-27 11:42:29 +0530

    Bhavesh Shah

--- src/Containers/Dashboard/Dashboard.js
+++ src/Containers/Dashboard/Dashboard.js
@@ -5,7 +5,7 @@ class Dashboard extends Component {
     render() {
         return(
             <Container maxWidth="md">
-                <h2>Welcome to Shree Nagari Society</h2>
+                <h2>Welcome to Shree Nagari Society...!</h2>
             </Container>
         )
     }
