9fed7ed77b2fe61992a1f053b9f82b2e075018d0
Bhavesh Shah <bhaveshkumar.shah@figmd.com>
2020-08-27 11:37:44 +0530

    Page has been added

---
+++ src/Containers/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
+
+class Dashboard extends Component {
+    render() {
+        return(
+            <Container maxWidth="md">
+                <h2>Welcome to Shree Nagari Society</h2>
+            </Container>
+        )
+    }
+}
+
+export default Dashboard;
